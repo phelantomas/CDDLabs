db5d4fa2f349fccfb94f7770abecc18b812a5c16
phelantomas <phelantomas@gmail.com>
2017-10-16 17:42:05 +0100

    updates

--- Lab04/Barrier.cpp
+++ Lab04/Barrier.cpp
@@ -6,17 +6,7 @@
 #include <iostream>
 #include <thread>
 
-void taskOne(std::shared_ptr<Semaphore> theSemaphore, std::shared_ptr<Semaphore> theBarrier, int *count, int N){
-  theSemaphore->Wait();
-  ++*count;
-  if(*count == N){
-    theBarrier->Signal();
-  }
-  theSemaphore->Signal();
-  theBarrier->Wait();
-  theBarrier->Signal();
-}
-void taskTwo(std::shared_ptr<Semaphore> theSemaphore, std::shared_ptr<Semaphore> theBarrier, int *count, int N){
+void task(std::shared_ptr<Semaphore> theSemaphore, std::shared_ptr<Semaphore> theBarrier, int *count, int N){
   theSemaphore->Wait();
   ++*count;
   if(*count == N){
@@ -34,8 +24,8 @@ int main(void){
   int count = 0;
   int N = 2;
   /**< Launch the threads  */
-  threadOne=std::thread(taskTwo, mutex, barrier, &count, N);
-  threadTwo=std::thread(taskOne,mutex, barrier, &count, N);
+  threadOne=std::thread(task, mutex, barrier, &count, N);
+  threadTwo=std::thread(task,mutex, barrier, &count, N);
   std::cout << "Launched from the main\n";
   threadOne.join();
   threadTwo.join();
