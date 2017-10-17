ed09de438000f8d76a7b4ea73322721bbe808dee
phelantomas <phelantomas@gmail.com>
2017-10-17 16:02:09 +0100

    Added comments and ouputs

--- Lab01/signal.cpp
+++ Lab01/signal.cpp
@@ -1,15 +1,20 @@
 #include "Semaphore.h"
 #include <iostream>
 #include <thread>
+//Autor: Tomas Phelan
+//Date: 17/10/17
+
 
 void taskOne(std::shared_ptr<Semaphore> theSemaphore){
   std::cout <<"I ";
   std::cout << "must ";
   std::cout << "print ";
   std::cout << "first"<<std::endl;
+  //allows the other task to now run
   theSemaphore->Signal();
 }
 void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
+  //prevents this task from running until told otherwise
   theSemaphore->Wait();
   std::cout <<"This ";
   std::cout << "will ";
