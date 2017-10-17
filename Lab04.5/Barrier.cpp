7f1ef7fff62b453cc209e61864e4647b4ad81a46
phelantomas <phelantomas@gmail.com>
2017-10-17 16:17:04 +0100

    Added comments and ouputs

--- Lab04.5/Barrier.cpp
+++ Lab04.5/Barrier.cpp
@@ -7,28 +7,33 @@
 #include <thread>
 
 void task(std::shared_ptr<Semaphore> theMutex, std::shared_ptr<Semaphore> turnstile,  std::shared_ptr<Semaphore> turnstile2, int *count, int N){
+  //allow only one task to increment count
   theMutex->Wait();
   ++*count;
   if(*count == N){
     turnstile2->Wait();
     turnstile->Signal();
+    std::cout << "Count is now " << *count << "equal to N so now allowed through first turnstile \n";
   }
   theMutex->Signal();
   
+  //Only allow througn when count is equal to N
   turnstile->Wait();
   turnstile->Signal();
 
+  std::cout << "Here is the critical section" << "\n";
   //critical stuff here
-
   theMutex->Wait();
 
   --*count;
   if(*count == 0){
     turnstile->Wait();
     turnstile2->Signal();
+    std::cout << "Count is now " << *count << " so now allowed through second turnstile \n";    
   }
   theMutex->Signal();
-  
+
+  //Will only allow to go through once all threads catch up
   turnstile2->Wait();
   turnstile2->Signal();
 }
