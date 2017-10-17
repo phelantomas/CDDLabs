208d7e41becf0999723384e858909d5e94108286
phelantomas <phelantomas@gmail.com>
2017-10-17 16:12:33 +0100

    Added comments and ouputs

--- Lab03/Mutex.cpp
+++ Lab03/Mutex.cpp
@@ -1,15 +1,24 @@
 #include "Semaphore.h"
 #include <iostream>
 #include <thread>
+//Author: Tomas Phelan
+//Date: 17/10/17
 
 void taskOne(std::shared_ptr<Semaphore> theSemaphore, int *count){
+  //only allows one task in at a time
   theSemaphore->Wait();
+  std::cout << "In critical section of task 1\n";
+  std::cout << "Count is now " << *count << " before being incremented\n";
   ++*count;
+  std::cout << "Count is now " << *count << " after being incremented\n";
   theSemaphore->Signal();
 }
 void taskTwo(std::shared_ptr<Semaphore> theSemaphore, int *count){
   theSemaphore->Wait();
+  std::cout << "In critical section of task 2\n";
+  std::cout << "Count is now " << *count << " before being incremented\n";
   ++*count;
+  std::cout << "Count is now " << *count << " after being incremented\n";
   theSemaphore->Signal();
 }
 
@@ -18,6 +27,7 @@ int main(void){
   std::shared_ptr<Semaphore> mutex(new Semaphore(1));
   int count = 0;
   /**< Launch the threads  */
+  std::cout << "Increment Count from 0 to 2 " << "\n";
   threadOne=std::thread(taskTwo, mutex, &count);
   threadTwo=std::thread(taskOne,mutex, &count);
   std::cout << "Launched from the main\n";
