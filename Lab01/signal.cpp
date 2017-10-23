45c4d7baf226bbb587f7829eb2762881fa7faa91
phelantomas <phelantomas@gmail.com>
2017-10-23 11:33:46 +0100

    Added doxygen comments

--- Lab01/signal.cpp
+++ Lab01/signal.cpp
@@ -1,20 +1,21 @@
+/*!
+Autor: Tomas Phelan
+Date: 17/10/17
+*/
 #include "Semaphore.h"
 #include <iostream>
 #include <thread>
-//Autor: Tomas Phelan
-//Date: 17/10/17
-
 
 void taskOne(std::shared_ptr<Semaphore> theSemaphore){
   std::cout <<"I ";
   std::cout << "must ";
   std::cout << "print ";
   std::cout << "first"<<std::endl;
-  //allows the other task to now run
+  /*! Allows the other task to now run */
   theSemaphore->Signal();
 }
 void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
-  //prevents this task from running until told otherwise
+  /*! Prevents this task from running until told otherwise */
   theSemaphore->Wait();
   std::cout <<"This ";
   std::cout << "will ";
@@ -25,7 +26,7 @@ void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
 int main(void){
   std::thread threadOne, threadTwo;
   std::shared_ptr<Semaphore> sem( new Semaphore);
-  /**< Launch the threads  */
+  /*! Launch the threads  */
   threadOne=std::thread(taskTwo,sem);
   threadTwo=std::thread(taskOne,sem);
   std::cout << "Launched from the main\n";
