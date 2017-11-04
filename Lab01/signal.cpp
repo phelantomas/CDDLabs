1a447e5dd23e73725611686a623b4315a7baee6e
phelantomas <phelantomas@gmail.com>
2017-11-04 16:59:39 +0000

    Added extra comments

--- Lab01/signal.cpp
+++ Lab01/signal.cpp
@@ -1,7 +1,49 @@
-/*!
-Autor: Tomas Phelan
-Date: 17/10/17
-*/
+// signal.cpp --- 
+// 
+// Filename: signal.cpp
+// Description: 
+// Author: Tomas Phelan
+// Maintainer: 
+// Version: 
+// Package-Requires: ()
+// Last-Updated: Sat Nov  4 16:58:45 2017 (+0000)
+//           By: Tomas Phelan
+//     Update #: 2
+// URL: 
+// Doc URL: 
+// Keywords: 
+// Compatibility: 
+// 
+// 
+
+// Commentary: 
+// 
+// 
+// 
+// 
+
+// Change Log:
+// 
+// 
+// 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// 
+
+// Code:
+
 #include "Semaphore.h"
 #include <iostream>
 #include <thread>
