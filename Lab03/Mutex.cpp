de371b50124488a3de5654b84ecf95d616917912
phelantomas <phelantomas@gmail.com>
2017-11-04 17:04:15 +0000

    Added extra comments

--- Lab03/Mutex.cpp
+++ Lab03/Mutex.cpp
@@ -1,7 +1,48 @@
-/*!
-Author: Tomas Phelan
-Date: 17/10/17
-*/
+// Mutex.cpp --- 
+// 
+// Filename: Mutex.cpp
+// Description: 
+// Author: Tomas Phelan
+// Maintainer: 
+// Version: 
+// Package-Requires: ()
+// Last-Updated: Sat Nov  4 17:03:48 2017 (+0000)
+//           By: Tomas Phelan
+//     Update #: 3
+// URL: 
+// Doc URL: 
+// Keywords: 
+// Compatibility: 
+// 
+// 
+
+// Commentary: 
+// 
+// 
+// 
+// 
+
+// Change Log:
+// 
+// 
+// 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// 
+
+// Code:
 
 #include "Semaphore.h"
 #include <iostream>
