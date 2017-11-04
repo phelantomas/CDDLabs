7b1591bd4126d9f7978943879314ee77ea9fd591
phelantomas <phelantomas@gmail.com>
2017-11-04 17:01:45 +0000

    Added extra comments

--- Lab02/rendezvous.cpp
+++ Lab02/rendezvous.cpp
@@ -1,11 +1,54 @@
+// rendezvous.cpp --- 
+// 
+// Filename: rendezvous.cpp
+// Description: 
+// Author: Tomas Phelan
+// Maintainer: 
+// Created: Wed Oct 25 12:53:19 2017 (+0100)
+// Version: 
+// Package-Requires: ()
+// Last-Updated: Sat Nov  4 17:01:19 2017 (+0000)
+//           By: Tomas Phelan
+//     Update #: 2
+// URL: 
+// Doc URL: 
+// Keywords: 
+// Compatibility: 
+// 
+// 
+
+// Commentary: 
+// 
+// 
+// 
+// 
+
+// Change Log:
+// 
+// 
+// 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// 
+
+// Code:
+
 #include "Semaphore.h"
 #include <iostream>
 #include <thread>
-/*!
-Author: Tomas Phelan
-Date: 23/10/2017
-Task: B1 always print before A2, and A1 always print before B2. 
-*/
+
 void taskOne(std::shared_ptr<Semaphore> taskOneSem,std::shared_ptr<Semaphore> taskTwoSem){
   std::cout << "A1 Prints Before B2 "<<std::endl;
   /*! Will now allow B2 to print */
